f946d96f369dc3fb97682c7a76c97d217f0aa3a7
Rafał Zubala <rafal.zubala.extern@ergo.com>
2023-05-11 13:04:35 +0200

    websocket server

---
+++ pages/socket/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import io, { Socket } from 'Socket.IO-client'
+let socket;
+
+const Home = () => {
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    socketInitializer()
+  }, [])
+
+  const socketInitializer = async () => {
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+
+    socket.on('status', msg => {
+      setInput(msg);
+    })
+
+    socket.on('update-input', msg => {
+      console.log('update-input', msg)
+      setInput(msg)
+    })
+  }
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value)
+    socket.emit('input-change', e.target.value)
+  }
+
+  return (
+    <input
+      placeholder="Type something"
+      value={input}
+      onChange={onChangeHandler}
+    />
+  )
+}
+
+export default Home;
